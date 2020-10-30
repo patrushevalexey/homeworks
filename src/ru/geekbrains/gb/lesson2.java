2e1a18ab5b8fa20c318b7ba3d944da31fb660358
patrushevalexey <patrushevalexey@ya.ru>
2020-10-30 03:04:07 +0300

    Домашняя работа ко 2 уроку.

---
+++ src/ru/geekbrains/gb/lesson2.java
@@ -0,0 +1,50 @@
+package ru.geekbrains.gb;
+
+public class lesson2 {
+
+    public static void main(String[] args) {
+        taskOne();
+        taskTwo();
+        taskThree();
+        taskfour();
+        taskfive();
+        tasksix();
+        taskseven();
+    }
+
+    static void taskOne() {
+        System.out.println("Задание №1");
+        System.out.println();
+    }
+
+    static void taskTwo() {
+        System.out.println("Задание №2");
+        System.out.println();
+    }
+
+    static void taskThree() {
+        System.out.println("Задание №3");
+        System.out.println();
+    }
+
+    static void taskfour() {
+        System.out.println("Задание №4");
+        System.out.println();
+    }
+
+    static void taskfive() {
+        System.out.println("Задание №5");
+        System.out.println();
+    }
+
+    static void tasksix() {
+        System.out.println("Задание №6");
+        System.out.println();
+    }
+
+    static void taskseven() {
+        System.out.println("Задание №7");
+        System.out.println();
+    }
+
+}
