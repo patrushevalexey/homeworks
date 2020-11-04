b927d5bee0c7eeb9095aad4e5bcb6de3983b6a51
patrushevalexey <patrushevalexey@ya.ru>
2020-11-05 02:37:30 +0300

    Домашняя работа ко 2 уроку.

--- src/ru/geekbrains/gb/lesson2.java
+++ src/ru/geekbrains/gb/lesson2.java
@@ -5,47 +5,57 @@ import java.util.Arrays;
 public class lesson2 {
 
     public static void main(String[] args) {
-        taskOne();
-        taskTwo();
-        taskThree();
-        taskfour();
-        taskfive();
-        tasksix();
-        taskseven();
+        changeZeroAndOne();
+        listWithStepPlusThree();
+        listEntityMultiplyTwo();
+        changeDiagonal();
     }
 
-    static void taskOne() {
+    static void changeZeroAndOne() {
         System.out.println("Задание №1");
+        int[] listZeroAndOne = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        int i;
+        for (i=0; i< listZeroAndOne.length; i++){
+            listZeroAndOne[i] = 1 - listZeroAndOne[i];
+        }
+        System.out.println(Arrays.toString(listZeroAndOne));
         System.out.println();
     }
 
-    static void taskTwo() {
+    static void listWithStepPlusThree() {
         System.out.println("Задание №2");
+        int[] listPlusThree = new int[8];
+        int i;
+        for (i=0; i< listPlusThree.length; i++){
+            listPlusThree[i] = i*3;
+        }
+        System.out.println(Arrays.toString(listPlusThree));
         System.out.println();
     }
 
-    static void taskThree() {
+    static void listEntityMultiplyTwo() {
         System.out.println("Задание №3");
+        int[] entityMultiplyTwo = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        int i;
+        for (i=0; i< entityMultiplyTwo.length; i++){
+            if (entityMultiplyTwo[i]<6){
+                entityMultiplyTwo[i] *= 2;
+            }
+        }
+        System.out.println(Arrays.toString(entityMultiplyTwo));
         System.out.println();
     }
 
-    static void taskfour() {
+    static void changeDiagonal() {
         System.out.println("Задание №4");
+        int[][] listDoubleDiagonal = new int[10][10];
+        int i;
+        for (i=0; i< listDoubleDiagonal.length; i++){
+            listDoubleDiagonal[i][i] = 1;
+            listDoubleDiagonal[i][listDoubleDiagonal.length - 1 - i] = 1;
+            System.out.println(Arrays.toString(listDoubleDiagonal[i]));
+        }
         System.out.println();
     }
 
-    static void taskfive() {
-        System.out.println("Задание №5");
-        System.out.println();
     }
-
-    static void tasksix() {
-        System.out.println("Задание №6");
-        System.out.println();
-    }
-
-    static void taskseven() {
-        System.out.println("Задание №7");
-    }
-
-}
